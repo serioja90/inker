117043054dda5a407f2a14bc5e5728a77ab27bb2
Groza Sergiu <groza@opinioni.net>
2022-12-20 18:32:11 +0100

    NEW: implemented the feature which allows to calculate contrast ratio between 2 `Inker::Color` and to calculate the result of 2 `Inker::Color` overlay + made the code more complaint to rubocop style guide

--- lib/inker/color/serializers.rb
+++ lib/inker/color/serializers.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 module Inker
   class Color
     # This module implements the methods which can be used to serialize
     # a color as string in different formats.
     module Serializers
-
       # Convert color to a HEX color string.
       #
       # @param force_alpha [Boolean] indicates if alpha channel should be included in HEX color string
@@ -11,32 +12,29 @@ module Inker
       # @return [String] a HEX color string
       def hex(force_alpha: false)
         result = hex6
-        result += (alpha * 255).to_i.to_s(16).rjust(2, "0") if alpha < 1 or force_alpha
+        result += (alpha * 255).to_i.to_s(16).rjust(2, '0') if alpha < 1 || force_alpha
 
-        return result
+        result
       end
-
 
       # Convert color to a HEX color string without alpha channel.
       #
       # @return [String] a HEX color string without alpha channel
       def hex6
         result = "#"
-        result += red.to_s(16).rjust(2, "0")
-        result += green.to_s(16).rjust(2, "0")
-        result += blue.to_s(16).rjust(2, "0")
+        result += red.to_s(16).rjust(2, '0')
+        result += green.to_s(16).rjust(2, '0')
+        result += blue.to_s(16).rjust(2, '0')
 
-        return result
+        result
       end
-
 
       # Convert color to RGB color string.
       #
       # @return [String] a RGB color string
       def rgb
-        return "rgb(#{red}, #{green}, #{blue})"
+        "rgb(#{red}, #{green}, #{blue})"
       end
-
 
       # Convert color to RGBA color string.
       #
@@ -44,17 +42,15 @@ module Inker
       #
       # @return [String] a RGBA color string
       def rgba(alpha_precision: 2)
-        return "rgba(#{red}, #{green}, #{blue}, #{alpha.round(alpha_precision)})"
+        "rgba(#{red}, #{green}, #{blue}, #{alpha.round(alpha_precision)})"
       end
-
 
       # Convert color to HSL color string.
       #
       # @return [String] a HSL color string
       def hsl
-        return "hsl(#{hue}, #{(saturation * 100).round}%, #{(lightness * 100).round}%)"
+        "hsl(#{hue}, #{(saturation * 100).round}%, #{(lightness * 100).round}%)"
       end
-
 
       # Convert color to HSL color string.
       #
@@ -62,7 +58,7 @@ module Inker
       #
       # @return [String] a HSL color string
       def hsla(alpha_precision: 2)
-        return "hsl(#{hue}, #{(saturation * 100).round}%, #{(lightness * 100).round}%, #{alpha.round(alpha_precision)})"
+        "hsl(#{hue}, #{(saturation * 100).round}%, #{(lightness * 100).round}%, #{alpha.round(alpha_precision)})"
       end
     end
   end
